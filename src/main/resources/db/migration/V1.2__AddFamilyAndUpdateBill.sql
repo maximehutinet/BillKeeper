aef20bb0ad6cf81dc5e9218a02e4e5c421c80a2e
Maxime Hutinet <maxime@hutinet.fr>
2025-04-28 02:49:26 +0200

    Fixed issue with migration file.

--- src/main/resources/db/migration/V1.2__AddFamilyAndUpdateBill.sql
+++ src/main/resources/db/migration/V1.2__AddFamilyAndUpdateBill.sql
@@ -52,9 +52,6 @@ UPDATE document
 SET user_id = b.user_id
 FROM bill b WHERE document.bill_id = b.id;
 
-ALTER TABLE document
-    ALTER COLUMN user_id SET NOT NULL;
-
 ALTER TABLE submission
     ADD user_id UUID
         CONSTRAINT fk_user_id REFERENCES billkeeperuser;
